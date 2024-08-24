74f8d615ec3bcd0d409be408e51d7825fe26a7dc
Aymar <ayymarsilva@gmail.com>
2024-08-23 23:07:36 -0300

    Correcoes

--- js/flipCard.js
+++ js/flipCard.js
@@ -2,37 +2,52 @@ let produto = document.getElementsByClassName("produto");
 const prodFormatado = Array.from(produto);
 
 function flipAction(objeto) {
-    let borda = objeto.querySelector('.borda');
+    let cartao = objeto.querySelector('.cartao');
     let imagem = objeto.querySelector('.produto-img').querySelector('img');
     let info = objeto.querySelector('.produto-info');
+
+    console.log("cliquei: ",cartao)
 
     console.log(imagem);
     console.log(info.querySelectorAll("*"));
 
-    borda.classList.toggle("flip");
-    imagem.classList.toggle("opacidade0")
-    // info.classList.toggle("flip");
-    info.classList.toggle("opacidade0");
+    cartao.classList.toggle("flip");
+    setTimeout(() => {
+        imagem.classList.toggle("opacidade0");
+        info.classList.toggle("opacidade0"); 
+    }, 100);
 
-    // imagem.querySelector("*").classList.toggle("opacidade0");
-    info.querySelectorAll("*").forEach(element => {
-        element.classList.toggle("opacidade0");
-    });
+    let abaDescricao = objeto.querySelector('.produto-img').querySelector(".descricao");
+    abaDescricao.classList.toggle("opacidade1");
 
-    // let div = objeto.querySelector('.produto-img');
-    // div.classList.toggle('borda_espac');
-    // div.classList.toggle('flip');
-    let descricao = objeto.querySelector('.produto-img').querySelector(".descricao");
-    descricao.classList.toggle("opacidade1");
-    objeto.querySelector('.produto-img').style.borderBottomLeftRadius = "20px";
-    objeto.querySelector('.produto-img').style.borderBottomRightRadius = "20px";
+    if(cartao.classList.contains("flip")){
+        objeto.querySelector('.produto-img').style.borderBottomLeftRadius = "20px";
+        objeto.querySelector('.produto-img').style.borderBottomRightRadius = "20px"; 
+    }else{
+        objeto.querySelector('.produto-img').style.borderBottomLeftRadius = "0";
+        objeto.querySelector('.produto-img').style.borderBottomRightRadius = "0"; 
+    };
+};
 
-    let botao = document.getElementById('bt-comprar');
-    botao.style.marginTop = "6px";
+function showDescricao(objeto,event){
+    event.stopPropagation();
+    const descricao = objeto.querySelector('.descricao').querySelector('p');
+    const seta = objeto.querySelector('.icon-expandir');  
+
+    descricao.classList.toggle('altura');
+
+    if (seta.classList.contains('rodarCima')) {
+        seta.classList.remove("rodarCima");
+    }else{
+        seta.classList.add("rodarCima");
+    }
 };
 
 prodFormatado.forEach(item => {
-    item.addEventListener("click", function() {
-        flipAction(item);
+    item.querySelector(".cartao").addEventListener("click", function() {
+        flipAction(item); 
+    });
+    item.querySelector('.expandir').addEventListener("click", function() {
+        showDescricao(item,event);
     });
 });
